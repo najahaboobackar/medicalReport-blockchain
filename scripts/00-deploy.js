c4e7889e1c0852e2901aed78d444992b0dcb0f59
Shahid Zaman <shahidalloor27@gmail.com>
2024-04-13 23:24:20 +0530

    seeding

--- scripts/00-deploy.js
+++ scripts/00-deploy.js
@@ -1,21 +1,15 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  console.log("Deploying the smart contract...");
-
+  console.log("Deploying smart contract...");
   const Medical = await ethers.getContractFactory("MedicalRecords");
-  const accounts = await ethers.getSigners();
-
-  const medical = await Medical.connect(accounts[0]).deploy();
-
-  await medical.deployTransaction.wait(); // Wait for the deployment transaction to be mined
-
-  console.log(`Medical contract deployed at address: ${medical.address}`);
-
-  process.exit(0);
+  const medical = await Medical.deploy(); // Deploy the contract
+  console.log(`Medical is deployed at address: ${medical.address}`);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
